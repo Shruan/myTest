3b6efb6db44362db95c57954630996a15dbb7bbe
Shruan <qiushiyuan1994@qq.com>
2017-08-22 18:03:18 +0800

    新建组件

---
+++ sellByVue/src/components/VHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class='header'>
+    i am header
+    <div class="content-wrapper">
+      <div class="avatar">
+        <img width='64'heith='64' :src="seller.avatar" alt="">
+      </div>
+    </div>
+    <div class="">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    seller: {}
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
