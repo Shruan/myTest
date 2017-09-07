3c25d1f372e2815d382ee1bac1ade5050176a688
Shruan <qiushiyuan1994@qq.com>
2017-09-07 18:01:46 +0800

    G2饼状图组件实现

--- G2ChartUseByVue/src/router/index.js
+++ G2ChartUseByVue/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import LineTest from '@/pages/LineTest'
+import PieTest from '@/pages/PieTest'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: '线性图表',
       component: LineTest
+    },
+    {
+      path: '/pie',
+      name: '线性图表',
+      component: PieTest
     }
   ]
 })
